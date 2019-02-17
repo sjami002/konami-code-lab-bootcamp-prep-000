6a7060ea15924909c63ad2cd07320d414f3fc8ba
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 03:45:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,21 +11,33 @@ const codes = [
   "a"
 ];
 
-function init() {
+
   // your code here
-  var index = 0;
-  document.addEventListener('keydown', function (e) {
-    const key = parseInt(e.detail || e.which);
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-     if (key === code[index]) {
-      index++;
 
-       if (index === code.length - 1) {
-        window.alert("YOU DID IT!");
-        index = 0;
+
+   function init() {	function init() {
+    // your code here	  //
+    var index = 0
+    document.body.addEventListener('keydown', onKeyDownHandler);
+
+     function onKeyDownHandler(e) {
+      const key = parseInt(e.detail || e.which)
+      console.log('key', key)
+      if (key === code[index]) {
+        index++
+
+         if (index === code.length) {
+          alert("YOU DID IT!")
+          index = 0
+        }
+      } else {
+        index = 0
       }
-    } else {
-      index = 0;
     }
-  }, false);
-}	
+  }	}
+
+
+
+   init();
